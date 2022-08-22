429c32771e18784571eb20206f796837802389e2
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-22 00:07:06 -0700

    divisible by 5 and 11 or not

---
+++ divisible by 5 and 11 or not/script.js
@@ -0,0 +1,8 @@
+var a = prompt("Please enter the number");
+var newa = Number(a);
+if (newa % 5 == 0 && newa % 11 == 0) {
+    alert("number is divisible");
+}
+else {
+    alert("The number is not visible");
+}
---
+++ divisible by 5 and 11 or not/script.ts
@@ -0,0 +1,10 @@
+var a:string|null=prompt("Please enter the number");
+var newa:number=Number(a);
+if( newa%5==0 && newa%11==0 ){
+    alert("number is divisible");
+
+}
+else{
+    alert("The number is not visible");
+
+}
