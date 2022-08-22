b01fe892e1ae743b714fad7f28db47c17400b7b0
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-22 00:15:18 -0700

    evven or odd

---
+++ even or odd/script.js
@@ -0,0 +1,8 @@
+var a = prompt("Please enter the value");
+var newa = Number(a);
+if (newa % 2 == 0) {
+    alert(newa + " is even");
+}
+else {
+    alert(newa + " is odd");
+}
---
+++ even or odd/script.ts
@@ -0,0 +1,9 @@
+var a:string|null=prompt("Please enter the value");
+var newa:number=Number(a);
+if(newa%2==0)
+{
+    alert(newa + " is even");
+}
+else{
+    alert(newa + " is odd");
+}
