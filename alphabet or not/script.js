f037776b2df08b6f406b9bcf400fb7a51fc21ba4
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-22 00:53:32 -0700

    alphabet or not

---
+++ alphabet or not/script.js
@@ -0,0 +1,7 @@
+var ch = prompt("Please enter the alphabet");
+if (ch == "A" || ch == "B" || ch == "C" || ch == "D" || ch == "E" || ch == "F" || ch == "G" || ch == "H" || ch == "I" || ch == "G" || ch == "H" || ch == "I" || ch == "J" || ch == "K" || ch == "L" || ch == "M" || ch == "N" || ch == "P" || ch == "Q" || ch == "R" || ch == "S" || ch == "T" || ch == "U" || ch == "V" || ch == "W" || ch == "X" || ch == "Y" || ch == "Z") {
+    alert(ch + " is a alphabet");
+}
+else {
+    alert(ch + "is not the alphabet");
+}
