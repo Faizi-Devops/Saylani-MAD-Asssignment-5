90c310d7e080bce1c24bc3f6f22d5dc9bf14dd0a
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-21 23:46:14 -0700

    negative,positive,zero

---
+++ negative,positive,zero/script.js
@@ -0,0 +1,11 @@
+var a = prompt("Please enter the number");
+var newa = Number(a);
+if (newa > 0) {
+    alert("The number is positive");
+}
+else if (newa < 0) {
+    alert("The number  is negative");
+}
+else {
+    alert("The number is zero");
+}
---
+++ negative,positive,zero/script.ts
@@ -0,0 +1,13 @@
+var a:string|null=prompt("Please enter the number");
+let newa:number=Number(a);
+if(newa>0)
+{
+    alert("The number is positive")
+}
+else if(newa<0)
+{
+alert("The number  is negative");
+}
+else{
+    alert("The number is zero");
+}
