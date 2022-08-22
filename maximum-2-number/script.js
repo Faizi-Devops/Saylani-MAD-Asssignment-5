65889362aed807d654235edf6119a19e15437462
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-21 23:01:46 -0700

    maximum number

---
+++ maximum-2-number/script.js
@@ -0,0 +1,10 @@
+var a = prompt("PLease enter the number");
+var newa = Number(a);
+var b = prompt("PLease enter the number");
+var newb = Number(b);
+if (newa > newb) {
+    alert(newa + "is larger");
+}
+else {
+    alert(newb + "is larger");
+}
---
+++ maximum-2-number/script.ts
@@ -0,0 +1,14 @@
+var a:string|null=prompt("PLease enter the number");
+var newa:number=Number(a);
+
+var b:string|null=prompt("PLease enter the number");
+var newb:number=Number(b);
+
+if(newa>newb){
+
+    alert(newa + "is larger");
+
+}
+else{
+    alert(newb + "is larger");
+}
