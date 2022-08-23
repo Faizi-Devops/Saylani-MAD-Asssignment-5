459cc9d9d05a95f26c95cef7208c18a7816ec955
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-23 00:51:17 -0700

    alphabet, digit or special character

---
+++ alphabet, digit or special character/script.ts
@@ -0,0 +1,14 @@
+var alpha: any = prompt("Please enter the string");
+
+if (alpha >= "a" && alpha <= "z" || alpha >= "A" && alpha <= "Z") {
+    alert("The character is Alphabet");
+}
+
+else if (alpha>="0" && alpha<=9){
+
+    alert("The character is number")
+
+}
+else{
+    alert("The character is special character")
+}
