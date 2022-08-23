678be18c2a5a8753064aeb5f32aba8facf725fdd
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-23 01:27:23 -0700

    month days

---
+++ print number of days in that month/script.ts
@@ -0,0 +1,56 @@
+let num:string|null= prompt("Please enter the number");
+
+let b:any=Number(num);
+
+if(b=="1"){
+    alert("The Month is January and the days are 31")
+
+}
+else if(b=="2"){
+    alert("The Month is February and the days are 28 or 29")
+
+}
+else if(b=="3"){
+    alert("The Month is March and the days are 31")
+
+}
+else if(b=="4"){
+    alert("The Month is April and the days are 30")
+
+}
+else if(b=="5"){
+    alert("The Month is May and the days are 31")
+
+}
+else if(b=="6"){
+    alert("The Month is June and the days are 30")
+
+}
+else if(b=="7"){
+    alert("The Month is July and the days are 31")
+
+}
+else if(b=="8"){
+    alert("The Month is August and the days are 31")
+
+}
+else if(b=="9"){
+    alert("The Month is September and the days are 30")
+
+}
+else if(b=="10"){
+    alert("The Month is May and the days are 31")
+
+}
+else if(b=="11"){
+    alert("The Month is May and the days are 30")
+
+}
+else if(b=="12"){
+    alert("The Month is May and the days are 31")
+
+}
+
+else{
+    alert("Please enter the correct number and remember the number start from 1 and ending with 12")
+}
