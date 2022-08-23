e81b128e5ba59a229d06abc2737ce4cff7ebfbce
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-23 00:59:22 -0700

    uppercase or lowercase alphabet

---
+++ uppercase or lowercase alphabet/script.js
@@ -0,0 +1,7 @@
+var kuch = prompt("Please enter the alphabet");
+if (kuch >= "a" && kuch <= "z") {
+    alert("The alphabet is lowecase");
+}
+else {
+    alert("The alphabet is upercase");
+}
---
+++ uppercase or lowercase alphabet/script.ts
@@ -0,0 +1,8 @@
+var kuch:any=prompt("Please enter the alphabet");
+if(kuch>="a" && kuch<="z"){
+    alert("The alphabet is lowecase")
+     
+}
+else{
+    alert("The alphabet is upercase");
+}
