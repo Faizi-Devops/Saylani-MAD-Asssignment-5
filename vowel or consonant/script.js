9177eb7acabb0c1f9ab46adaf568ff6f476aed2f
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-23 00:14:33 -0700

    vowel or consonant

---
+++ vowel or consonant/script.js
@@ -0,0 +1,13 @@
+var alpha = prompt("Please enter an alphabet");
+if (alpha == "a" || alpha == "e" || alpha == "i" || alpha == "o" || alpha == "u") {
+    alert("The alphabet is vowel");
+}
+else if (alpha == "") {
+    alert("Please enter the alphabet");
+}
+else if (alpha = "b" || alpha == "c" || alpha == "d" || alpha == "f" || alpha == "g" || alpha == "h" || alpha == "j" || alpha == "k" || alpha == "l" || alpha == "m" || alpha == "n" || alpha == "p" || alpha == "r" || alpha == "s" || alpha == "t" || alpha == "u" || alpha == "v" || alpha == "w" || alpha == "x" || alpha == "y" || alpha == "z") {
+    alert("The alphabet is consonant");
+}
+else {
+    alert("any error");
+}
---
+++ vowel or consonant/script.ts
@@ -0,0 +1,25 @@
+var alpha:string|null =prompt("Please enter an alphabet");
+
+if(alpha=="a" || alpha=="e" || alpha=="i" || alpha=="o" || alpha=="u"){
+
+    alert("The alphabet is vowel")
+
+}
+else if (alpha==""){
+    alert("Please enter the alphabet");
+
+}
+
+else if(alpha="b" || alpha=="c" || alpha=="d" || alpha=="f" || alpha=="g" || alpha=="h" || alpha=="j" || alpha=="k" || alpha=="l" || alpha=="m" || alpha=="n" || alpha=="p" || alpha=="r" || alpha=="s"  || alpha=="t" || alpha=="u" || alpha=="v" || alpha=="w" || alpha=="x" || alpha=="y" || alpha=="z")
+{
+
+    alert("The alphabet is consonant");
+
+}
+
+else{
+    alert("any error");
+}
+
+
+
