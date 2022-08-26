282597af834f055fdcb75a7512a5db0d63ab311d
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-25 22:52:26 -0700

    changing

--- basic salary/script.js
+++ basic salary/script.js
@@ -2,18 +2,22 @@ var a = prompt("Please enter the Basic Salary of Employee");
 var b = Number(a);
 var HRA;
 var DA;
+var gross;
 if (b <= 10000) {
     HRA = (b / 100) * 20;
     DA = (b / 100) * 80;
-    alert("The HRA is " + HRA + " and DA is " + DA);
+    gross = b + HRA + DA;
+    alert("The HRA is " + HRA + " and DA is " + DA + " and the Gross Salaray is " + gross);
 }
 else if (b <= 20000) {
     HRA = (b / 100) * 25;
     DA = (b / 100) * 90;
-    alert("The HRA is " + HRA + " and DA is " + DA);
+    gross = b + HRA + DA;
+    alert("The HRA is " + HRA + " and DA is " + DA + " and the Gross Salaray is " + gross);
 }
 else {
     HRA = (b / 100) * 30;
     DA = (b / 100) * 95;
-    alert("The HRA is " + HRA + " and DA is " + DA);
+    gross = b + HRA + DA;
+    alert("The HRA is " + HRA + " and DA is " + DA + " and the Gross Salaray is " + gross);
 }
--- basic salary/script.ts
+++ basic salary/script.ts
@@ -1,34 +1,23 @@
-let a:string|null = prompt("Please enter the Basic Salary of Employee");
-let b :number = Number(a);
-
-let HRA:number;
-let DA:number;
-
-if(b<=10000)
-{
-
-    HRA=(b/100)*20;
-    DA=(b/100)*80;
-
-    alert("The HRA is " + HRA + " and DA is " + DA);
-
-
+var a:string|null = prompt("Please enter the Basic Salary of Employee");
+var b:number = Number(a);
+var HRA:number;
+var DA:number;
+var gross:number;
+if (b <= 10000) {
+    HRA = (b / 100) * 20;
+    DA = (b / 100) * 80;
+    gross = b+HRA+DA;
+    alert("The HRA is " + HRA + " and DA is " + DA + " and the Gross Salaray is " + gross);
 }
-else if (b<=20000){
-    HRA=(b/100)*25;
-    DA=(b/100)*90;
-
-    alert("The HRA is " + HRA + " and DA is " + DA);
-    
-
+else if (b <= 20000) {
+    HRA = (b / 100) * 25;
+    DA = (b / 100) * 90;
+    gross = b+HRA+DA;
+    alert("The HRA is " + HRA + " and DA is " + DA + " and the Gross Salaray is " + gross);
 }
-
-else{
-
-    HRA=(b/100)*30;
-    DA=(b/100)*95;
-
-    alert("The HRA is " + HRA + " and DA is " + DA);
-    
-
+else {
+    HRA = (b / 100) * 30;
+    DA = (b / 100) * 95;
+    gross = b+HRA+DA;
+    alert("The HRA is " + HRA + " and DA is " + DA +  " and the Gross Salaray is " + gross);
 }
